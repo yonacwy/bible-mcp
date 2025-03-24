aef690203e1fa8012201247b74610c763fe65e58
Mike Brinker <themikejr@gmail.com>
2025-03-24 10:33:29 -0400

    tsc, jest, ts-jest kind of working.

---
+++ src/db/database.test.ts
@@ -0,0 +1,13 @@
+import { getEnglishText } from "./database.js";
+
+test("Retrieve James 1:5 (English)", () => {
+  const result = getEnglishText({
+    book: "James",
+    bookId: "59",
+    chapter: 1,
+    verse: 5,
+    testament: "NT",
+    osisId: "Jas 1:5",
+  });
+  console.log(result);
+});
